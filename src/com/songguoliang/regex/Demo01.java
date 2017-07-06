2f13389c5c35e785650ce6747acc791948d9de65
guoguo <gnail_oug@163.com>
2017-07-06 20:31:53 +0800

    修改例子

--- src/com/songguoliang/regex/Demo01.java
+++ src/com/songguoliang/regex/Demo01.java
@@ -233,15 +233,31 @@ public class Demo01 {
 	public static void stringBoundary(){
 		String str="hello world,hello java,hello java";
 		
-		System.out.println("===========匹配字符串开头===========");
-		// ^表示匹配字符串的开头，但是如何在[]里面则表示非，如[^a-f] 不匹配a-f
-		Pattern p=Pattern.compile("^hello");
+		System.out.println("===========匹配字符串===========");
+		//匹配str中所有字符串hello，这时str中3个hello都能匹配上，通过下面打印的匹配上的字符串的位置可以看出
+		Pattern p=Pattern.compile("hello");
 		Matcher m=p.matcher(str);
 		while(m.find()){
 			System.out.println(m.group()+"   位置：["+m.start()+","+m.end()+"]");
 		}
+		System.out.println("===========匹配字符串，并且该字符串是在开头的位置===========");
+		// ^表示匹配字符串的开头，但是如何在[]里面则表示非，如[^a-f] 不匹配a-f
+		// "hello"和"^hello"的区别就是：前者匹配时不管是不是在开头位置，只要能匹配就行，后者则是不但要能匹配而且还要是在开头的位置。这时str中3个hello只有第1个能匹配上。
+		p=Pattern.compile("^hello");
+		m=p.matcher(str);
+		while(m.find()){
+			System.out.println(m.group()+"   位置：["+m.start()+","+m.end()+"]");
+		}
 		
-		System.out.println("===========匹配字符串结尾===========");
+		System.out.println("===========匹配字符串===========");
+		//这时str中两个java都能匹配上
+		p=Pattern.compile("java");
+		m=p.matcher(str);
+		while(m.find()){
+			System.out.println(m.group()+"   位置：["+m.start()+","+m.end()+"]");
+		}
+		System.out.println("===========匹配字符串，并且是该字符串是在末尾的位置===========");
+		//这时str中两个java只有第2个才能匹配上
 		p=Pattern.compile("java$");
 		m=p.matcher(str);
 		while(m.find()){
@@ -585,13 +601,13 @@ public class Demo01 {
 		//Demo01.greedMode();
 		//Demo01.caseInsensitive();
 		//Demo01.wordBoundary();
-		//Demo01.stringBoundary();
+		Demo01.stringBoundary();
 		//Demo01.multiline();
 		//Demo01.subexpression();
 		//Demo01.group();
 		//Demo01.backreference();
 		//Demo01.replace();
-		Demo01.lookaround();
+		//Demo01.lookaround();
 		
 		
 		
